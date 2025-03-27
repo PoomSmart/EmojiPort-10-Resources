a301f2ee398ee8abeca4e3fd06be2464a4dd65d8
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-03-27 21:57:48 +0700

    Stable iOS 18.4 assets

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 TARGET := iphone:clang:latest:10.0
-PACKAGE_VERSION = 1.7.0~b1
 
 include $(THEOS)/makefiles/common.mk
 
--- renamer.sh
+++ renamer.sh
@@ -1,12 +1,15 @@
 STAGE_PATH=$1
 
+rm -f $STAGE_PATH/CoreEmoji
 rm -f $STAGE_PATH/*.lproj/InfoPlist.strings
 rm -f $STAGE_PATH/Info.plist
 rm -rf $STAGE_PATH/_CodeSignature
+for f in $(find $STAGE_PATH -type f -name '*2.bitmap'); do rm -f "$f"; done
 for f in $(find $STAGE_PATH -type f -name '*2.dat'); do rm -f "$f"; done
 for f in $(find $STAGE_PATH -type f -name '*2.strings'); do rm -f "$f"; done
 for f in $(find $STAGE_PATH -type f -name '*2.stringsdict'); do rm -f "$f"; done
 for f in $(find $STAGE_PATH -type f -name '*2.plist'); do rm -f "$f"; done
+for f in $(find $STAGE_PATH -type f -name '*.bitmap'); do mv "$f" "${f%.*}2.bitmap"; done
 for f in $(find $STAGE_PATH -type f -name '*.dat'); do mv "$f" "${f%.*}2.dat"; done
 for f in $(find $STAGE_PATH -type f -name '*.strings'); do mv "$f" "${f%.*}2.strings"; done
 for f in $(find $STAGE_PATH -type f -name '*.stringsdict'); do mv "$f" "${f%.*}2.stringsdict"; done
