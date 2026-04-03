2aae0fb19785f34683d082f91b25521c08d86373
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2026-04-03 22:39:51 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ PKG="$DIR/packages"
 
 make -C "$DIR"             clean package FINALPACKAGE=1 THEOS_PACKAGE_DIR="$PKG" "$@"
 make -C "$DIR"             clean package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless THEOS_PACKAGE_DIR="$PKG" "$@"
+make -C "$DIR/assets-16"   clean package FINALPACKAGE=1 THEOS_PACKAGE_DIR="$PKG" "$@"
 make -C "$DIR/assets-16"   clean package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless THEOS_PACKAGE_DIR="$PKG" "$@"
 make -C "$DIR/assets-17"   clean package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless THEOS_PACKAGE_DIR="$PKG" "$@"
